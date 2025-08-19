38d89639c4405296bc302f0aef582edc8042806e
João Duarte <joao84838@gmail.com>
2025-08-19 10:54:11 -0300

    Adição do "Use" e "mount" no main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { routerKey } from 'vue-router'
 
 createApp(App).mount('#app')
+    .use(router)
+
+    .mount('#app')
+    
